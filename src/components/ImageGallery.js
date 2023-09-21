95de0ebc823d7afdaeab37ae9f52f2519f42cca5
koome <frankoomef@gmail.com>
2023-09-21 12:59:41 +0300

    refined search to more matching options

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Fuse from 'fuse.js'
+import { FcSearch } from 'react-icons/fc'
 
 const ItemType = 'IMAGE';
 
@@ -16,7 +17,7 @@ const DraggableImage = ({ image, index, moveImage }) => {
     type: ItemType,
     item: { index },
   });
-
+  // Change position of image on drop
   const [, drop] = useDrop({
     accept: ItemType,
     hover: (draggedImage) => {
@@ -27,9 +28,10 @@ const DraggableImage = ({ image, index, moveImage }) => {
     },
   });
   
+  // blurdata for the image skeleton placeholder
   const blurData = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIklEQVR4nGNgMHaUC0nm9ktm4FVh2Hn+yPPfb21jMvX94wBhmgkwGRIDgwAAAABJRU5ErkJggg=='
   return (
-    <div ref={(node) => ref(drop(node))}  className='ease-in-out hover:scale-110'>
+    <div ref={(node) => ref(drop(node))}  className='ease-in-out hover:scale-105'>
       <div className='rounded-md border-2 border-gray-600 h-full'>
         <Image 
         title={image}
@@ -49,16 +51,17 @@ const ImageGallery = ({ images }) => {
   const [galleryImages, setGalleryImages] = useState(images);
   const fuse = new Fuse(images, options);
   const [searchImage, setSearchImage] = useState('');
+  // Search function for filtering images according to typed criteria
   function getImages(e) {
     setSearchImage(e.target.value);
-    const foundImages = fuse.search(searchImage).map(element => element.item);
+    const foundImages = fuse.search(searchImage).filter(element => element.score < 0.3).map(element => element.item);
     if (searchImage.length === 0) {
       setGalleryImages(images);
     } else {
       setGalleryImages(foundImages);
     }
   }
-
+  // Drag and drop the images
   const moveImage = (fromIndex, toIndex) => {
     const updatedImages = [...galleryImages];
     const [movedImage] = updatedImages.splice(fromIndex, 1);
@@ -68,16 +71,21 @@ const ImageGallery = ({ images }) => {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <form className='grid p-px w-full'>
-        <input 
-        type='search'
-        placeholder='Type to search for images'
-        onKeyDown={getImages}
-        name='search'
-        className='bg-gray-900 justify-self-center text-xl m-4 text-center rounded-md border-2 border-white'
-        />
+      <form className='grid p-px w-full'
+      >
+        <div class='relative justify-self-center w-1/3'>
+          <input 
+          type='search'
+          placeholder='Type to search for images'
+          name='search'
+          onKeyUp={getImages}
+          className='bg-gray-900 text-xl m-4 text-center rounded-md border-2 border-white w-full'
+          />
+          <button className='absolute right-0 top-1/2 transform -translate-y-1/2' ><FcSearch size={24}/></button>
+        </div>
       </form>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {/* Create dragable image from array of images */}
         {galleryImages.map((image, index) => (
           <DraggableImage
             key={index}
